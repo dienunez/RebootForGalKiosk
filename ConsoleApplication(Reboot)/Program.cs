290dd87b03c7c42e946b7ae3248fbca92198985d
dienunez <nunez.diegoa@gmail.com>
2022-02-23 11:53:49 -0300

    First Commit

---
+++ ConsoleApplication(Reboot)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApplication_Reboot_
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Launch();
+            Console.ReadLine();
+        }
+        private static void Launch()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "C:\\Windows\\Galkiosk.exe";
+
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.EnableRaisingEvents = true;
+            p.Exited += LaunchAgain;
+
+            p.Start();
+        }
+
+        private static void LaunchAgain(object o, EventArgs e)
+        {
+            Console.WriteLine("Process was killed; launching again");
+            Launch();
+        }
+
+    }
+}
